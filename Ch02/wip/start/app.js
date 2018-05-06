afcb2afcdb6f1220deedb20a25abb75a9f70d097
Shivam Mehrotra <smehro4@uic.edu>
2018-05-06 04:54:35 +0000

    Added product image

--- Ch02/wip/start/app.js
+++ Ch02/wip/start/app.js
@@ -1,6 +1,13 @@
 (function() {
   "use strict";
 
-  var ProductCustomizer = React.createElement('div', { className: "customizer" }, "Product customizer will go here");
-  ReactDOM.render(ProductCustomizer, document.getElementById('react-root'));
+  function ProductImage(props) {
+    return React.createElement("img", { src: "../../../assets/red.jpg", alt: "Product image" });
+  }
+  
+  function ProductCustomizer(props) {
+    return React.createElement("div", { className: "customizer" }, React.createElement("div", { className: "product-image" }, React.createElement(ProductImage)));
+  }
+  
+  ReactDOM.render(React.createElement(ProductCustomizer), document.getElementById('react-root'));
 })();
