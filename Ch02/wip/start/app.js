12d8a7d23b00f525ad63c1d793cf8bcffde3539c
Shivam Mehrotra <smehro4@uic.edu>
2018-05-06 04:53:40 +0000

    Created dom for product customizer

--- Ch02/wip/start/app.js
+++ Ch02/wip/start/app.js
@@ -1,6 +1,6 @@
 (function() {
   "use strict";
 
-  // Start here
-
+  var ProductCustomizer = React.createElement('div', { className: "customizer" }, "Product customizer will go here");
+  ReactDOM.render(ProductCustomizer, document.getElementById('react-root'));
 })();
