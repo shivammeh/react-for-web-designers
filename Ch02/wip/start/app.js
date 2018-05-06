a0ab767c592b01725076e17bdbb867f8719b7fb6
Shivam Mehrotra <smehro4@uic.edu>
2018-05-06 04:58:18 +0000

    add size options

--- Ch02/wip/start/app.js
+++ Ch02/wip/start/app.js
@@ -1,6 +1,25 @@
 (function() {
   "use strict";
-
+  
+  function SizeSelector(props) {
+    function sizeOptions() {
+      return window.Inventory.allSizes.map(function(num) {
+        return (
+            <option value={num} key={num}>{num}</option>
+          );
+      });
+    }
+    
+    return (
+        <div className="field-group">
+          <label htmlFor="size-options">Size:</label>
+          <select name="sizeOptions" id="size-options">
+            	{sizeOptions()}
+          </select>
+        </div>
+      );
+  }
+  
   function ProductImage(props) {
     return <img src="../../../assets/red.jpg" alt="Product image"/>;
   }
@@ -10,6 +29,9 @@
       <div className="customizer">
         <div className="product-image">
           <ProductImage />
+        </div>
+        <div className="selectors">
+          <SizeSelector />
         </div>
       </div>
     );
