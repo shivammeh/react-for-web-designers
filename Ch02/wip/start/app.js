5e44ce5ad75109d38ae3fc398e25b7850d3a2dd3
Shivam Mehrotra <smehro4@uic.edu>
2018-05-06 04:55:25 +0000

    Added babel

--- Ch02/wip/start/app.js
+++ Ch02/wip/start/app.js
@@ -2,11 +2,17 @@
   "use strict";
 
   function ProductImage(props) {
-    return React.createElement("img", { src: "../../../assets/red.jpg", alt: "Product image" });
+    return <img src="../../../assets/red.jpg" alt="Product image"/>;
   }
   
   function ProductCustomizer(props) {
-    return React.createElement("div", { className: "customizer" }, React.createElement("div", { className: "product-image" }, React.createElement(ProductImage)));
+    return (
+      <div className="customizer">
+        <div className="product-image">
+          <ProductImage />
+        </div>
+      </div>
+    );
   }
   
   ReactDOM.render(React.createElement(ProductCustomizer), document.getElementById('react-root'));
